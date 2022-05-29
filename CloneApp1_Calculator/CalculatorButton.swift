68e0e0eed7f173e930f34162adc054260103b8de
era_i_ser <eraser3031@gmail.com>
2022-05-29 18:15:02 +0900

    feat: add textField

---
+++ CloneApp1_Calculator/CalculatorButton.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorButton.swift
+//  CloneApp1_Calculator
+//
+//  Created by 김예훈 on 2022/05/29.
+//
+
+import UIKit
+
+class CalculatorButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CloneApp1_Calculator/CalculatorViewController.swift
+++ CloneApp1_Calculator/CalculatorViewController.swift
@@ -12,7 +12,22 @@ class CalculatorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        
+        // 텍스트 필드
+        let textField = UITextField()
+        textField.textColor = .white
+        textField.font = .systemFont(ofSize: 72, weight: .light)
+        textField.text = "123"
+        textField.textAlignment = .right
+        view.addSubview(textField)
+        
+        let margins = view.layoutMarginsGuide
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        textField.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        textField.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        
+        // 버튼 (수학기호(가로모드), 숫자+., C/AC +/- &, 기본연산자) 4개 변종 - 기본 버튼 만들고 델리게이트로 해보자
     }
     
 
